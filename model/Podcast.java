93214a47ba6fa8d62be418a7ce65d976069899ba
mohammedsalmank04 <mohammedsalmank04@gmail.com>
2025-11-21 20:39:15 +0530

    add PodCast-class

--- model/Podcast.java
+++ model/Podcast.java
@@ -1,4 +1,39 @@
 package model;
 
 public class Podcast {
+
+    private String podcastName;
+    private int podcastlenMIN;
+
+    public Podcast() {
+    }
+
+    public Podcast(String podcastName, int podcastlenMIN) {
+        this.podcastName = podcastName;
+        this.podcastlenMIN = podcastlenMIN;
+    }
+
+    public String getPodcastName() {
+        return podcastName;
+    }
+
+    public void setPodcastName(String podcastName) {
+        this.podcastName = podcastName;
+    }
+
+    public int getPodcastlenMIN() {
+        return podcastlenMIN;
+    }
+
+    public void setPodcastlenMIN(int podcastlenMIN) {
+        this.podcastlenMIN = podcastlenMIN;
+    }
+
+    @Override
+    public String toString() {
+        return "Podcast{" +
+                "podcastName='" + podcastName + '\'' +
+                ", podcastlenMIN=" + podcastlenMIN +
+                '}';
+    }
 }
